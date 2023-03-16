2cbce11e666906d41495f5503f6c3cfa0647a74a
aymanali00200 <aymanali002@gmail.com>
2023-03-16 10:41:24 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,16 @@
 /**
  * main - Entry point
  *
- * Description : A C program that prints without printf or puts
+ * Description: A C program that prints without printf or puts
  *
- * Return : Always 1
+ * Return: Return an error 1
 */
 
 int main(void)
 {
-	char msg[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1, msg, 59);
 	return (1);
 }
+
